5f9418850f5d730457b245a96b0dae2bd6a461fa
Gabriel Soares <danilo.estudante546@gmail.com>
2024-06-17 09:13:51 -0300

    primeirosite

---
+++ main.js
@@ -0,0 +1,8 @@
+let vermelho = document.getElementById("red");
+let blue= document.getElementById("blue");
+let green= document.getElementById("green");
+let fundo = document.getElementById("tela");
+
+function corVermelha(){
+    fundo.style.backgroundColor = "red";
+}
